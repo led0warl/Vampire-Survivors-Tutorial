548355ca1afe9cd16515662cfdc3af177b3a413e
led0warl <led0warl@hotmail.com>
2023-02-24 16:46:14 +0700

    StatSystem:LevelSystem and TestController

---
+++ Assets/LevelSystem/Scripts/Editor/LevelNodeView.cs
@@ -0,0 +1,21 @@
+using Core;
+using Core.Editor;
+using Core.Editor.Nodes;
+using LevelSystem.Nodes;
+using UnityEngine;
+
+namespace LevelSystem.Editor
+{
+    [NodeType(typeof(LevelNode))]
+    [Title("Level System", "Level")]
+    public class LevelNodeView : NodeView
+    {
+        public LevelNodeView()
+        {
+            title = "Level";
+            node = ScriptableObject.CreateInstance<LevelNode>();
+            output = CreateOutputPort();
+        }
+    }
+
+}
---
+++ Assets/LevelSystem/Tests/Runtime/LevelControllerTests.cs
@@ -0,0 +1,31 @@
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+
+namespace LevelSystem.Tests
+{
+    public class LevelControllerTests 
+    {
+        [OneTimeSetUp]
+        public void OneTimeSetup()
+        {
+            EditorSceneManager.LoadSceneInPlayMode("Assets/LevelSystem/Tests/Scenes/Tests.unity",new LoadSceneParameters(LoadSceneMode.Single));
+        }
+
+        [UnityTest]
+        public IEnumerator LevelController_WhenLevelUp_UpdateRequiredExperience()
+        {
+            yield return null;
+            LevelController levelController = GameObject.FindObjectOfType<LevelController>();
+            Assert.AreEqual(1, levelController.level);
+            levelController.currentExperience += 83;
+            Assert.AreEqual(2, levelController.level);
+            Assert.AreEqual(0, levelController.currentExperience);
+            Assert.AreEqual(92, levelController.requiredExperience);
+        }
+    }
+}
