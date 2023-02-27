4be56e2a1b9fb97f5d849151a7e9e4f0288c2547
led0warl <led0warl@hotmail.com>
2023-02-27 23:11:19 +0700

    StatSystem:GamePlay-implement MoveSpeed

--- Assets/MyGame/Scripts/Runtime/Player.cs
+++ Assets/MyGame/Scripts/Runtime/Player.cs
@@ -3,19 +3,32 @@ using StatSystem;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 namespace MyGame
 {
     [RequireComponent(typeof(PlayerStatController))]
+    [RequireComponent(typeof(NavMeshAgent))]
     public class Player : MonoBehaviour
     {
         private PlayerStatController m_PlayerStatController;
         private ILevelable m_Levelable;
+        [SerializeField] private Transform m_Target;
+        private NavMeshAgent m_NavMeshAgent;
 
         private void Awake()
         {
             m_PlayerStatController= GetComponent<PlayerStatController>();
             m_Levelable = m_PlayerStatController.GetComponent<ILevelable>();
+            m_NavMeshAgent = GetComponent<NavMeshAgent>();
+        }
+
+        private void Start()
+        {
+            if (m_Target != null)
+            {
+                m_NavMeshAgent.SetDestination(m_Target.position);
+            }
         }
 
         private void Update()
